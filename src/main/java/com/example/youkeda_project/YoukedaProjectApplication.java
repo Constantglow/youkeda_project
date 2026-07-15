589cb1a5fde2176040f5bdee5c0188267e17066c
hyb666666 <2959027353@qq.com>
2026-07-15 16:43:12 +0800

    1

---
+++ src/main/java/com/example/youkeda_project/YoukedaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.youkeda_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoukedaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoukedaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/youkeda_project/YoukedaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youkeda_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoukedaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
